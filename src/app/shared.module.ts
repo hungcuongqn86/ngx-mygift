9750f5a24014d43992f4bcf953476b88a46e6d92
Nguyen Hung Cuong <hungcuongqn86@gmail.com>
2018-06-24 15:58:46 +0700

    base

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -1,12 +1,13 @@
 import {ModuleWithProviders, NgModule} from '@angular/core';
 import {MessageDirective} from './messages/message.directive';
 import {PaginationModule} from 'ngx-bootstrap/pagination';
+import {ProgressbarModule} from 'ngx-bootstrap';
 import {ModalModule} from 'ngx-bootstrap/modal';
 
 @NgModule({
-    imports: [PaginationModule.forRoot(), ModalModule.forRoot()],
+    imports: [PaginationModule.forRoot(), ModalModule.forRoot(), ProgressbarModule.forRoot()],
     declarations: [MessageDirective],
-    exports: [MessageDirective, PaginationModule, ModalModule],
+    exports: [MessageDirective, PaginationModule, ModalModule, ProgressbarModule],
     providers: []
 })
 export class SharedModule {
